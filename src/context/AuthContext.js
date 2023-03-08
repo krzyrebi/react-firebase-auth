e85249b45d3234b9ec79664119dc6a4576506b7e
krzy rebi <petarok44@gmail.com>
2023-03-08 10:59:33 +0100

    corrected observer

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { auth } from '../firebase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updatePassword, updateEmail } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updatePassword, updateEmail, onAuthStateChanged } from "firebase/auth";
 
 
 const AuthContext = createContext();
@@ -39,7 +39,7 @@ export const AuthProvider = ({ children }) => {
 
 
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
             setCurrentUser(user);
             setLoading(false);
         })
